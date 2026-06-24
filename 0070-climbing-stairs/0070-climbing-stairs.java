34f52e2eb73c576d1055da4684cc004bfa0b66de
Omkar Manve <160436702+omkarmanve@users.noreply.github.com>
2026-06-24 07:32:09 +0530

    Time: 0 ms (100%), Space: 42.1 MB (34.92%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
+
+          if(n<=2)
+        return n;
         
         int a=1;
         int b=1;
         int c=0;
 
-        if(n<=2)
-        return n;
+      
 
         
 
