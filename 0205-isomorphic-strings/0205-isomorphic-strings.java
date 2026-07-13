a90e591f63209443640cfd909c970a56527587e7
Omkar Manve <160436702+omkarmanve@users.noreply.github.com>
2026-07-13 22:49:59 +0530

    Time: 3 ms (99.6%), Space: 44.1 MB (15.98%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,37 +1,20 @@
-import java.util.HashMap;
-
 class Solution {
     public boolean isIsomorphic(String s, String t) {
 
         char[] arr1 = s.toCharArray();
         char[] arr2 = t.toCharArray();
 
-        HashMap<Character, Character> map1 = new HashMap<>();
-        HashMap<Character, Character> map2 = new HashMap<>();
+        int[] map1 = new int[256];
+        int[] map2 = new int[256];
 
         for (int i = 0; i < arr1.length; i++) {
 
-            // s -> t mapping
-            if (map1.containsKey(arr1[i])) {
-
-                if (map1.get(arr1[i]) != arr2[i]) {
-                    return false;
-                }
-
-            } else {
-                map1.put(arr1[i], arr2[i]);
+            if (map1[arr1[i]] != map2[arr2[i]]) {
+                return false;
             }
 
-            // t -> s mapping
-            if (map2.containsKey(arr2[i])) {
-
-                if (map2.get(arr2[i]) != arr1[i]) {
-                    return false;
-                }
-
-            } else {
-                map2.put(arr2[i], arr1[i]);
-            }
+            map1[arr1[i]] = i + 1;
+            map2[arr2[i]] = i + 1;
         }
 
         return true;
